242603bceaf2a6d585c3cabcf56be53418380c65
Maaack <marek.belski@gmail.com>
2020-05-02 14:05:55 -0700

    GAME-03: Add sounds and music.

--- Scenes/GameLayer/IntrusionLevels/Chapter_0/Level_0_0.gd
+++ Scenes/GameLayer/IntrusionLevels/Chapter_0/Level_0_0.gd
@@ -8,6 +8,8 @@ const FOCUS_TOOL_ANIMATION = 'Focus'
 
 onready var unlocks_count_node = $ToolsNode/UnlocksCounter/UnlocksCount
 onready var animation_node = $ToolsNode/AnimationPlayer
+onready var connect_audio_node = $ConnectAudio
+onready var error_audio_node = $ErrorAudio
 
 export(int) var starting_unlocks : int = 1 setget set_starting_unlocks
 
@@ -24,7 +26,15 @@ func _connect_player_to_node(node:IntrusionNode):
 	if _can_unlock(node):
 		var connected = ._connect_player_to_node(node)
 		if connected:
+			if is_instance_valid(connect_audio_node):
+				connect_audio_node.play()
 			_remove_unlock(node)
+		else:
+			if is_instance_valid(error_audio_node):
+				error_audio_node.play()
+	else:
+		if is_instance_valid(error_audio_node):
+			error_audio_node.play()
 
 func _can_unlock(node:IntrusionNode) -> bool :
 	if node.is_in_group('GREEN_NODE'):
